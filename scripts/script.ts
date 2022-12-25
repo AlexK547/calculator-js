87dcb3097a5171d72ef6b9c184171a45dbbc7fde
Alex Kozhenov <kozhenovalex@gmail.com>
2022-12-25 04:55:28 +0300

    add event keyboard

--- scripts/script.ts
+++ scripts/script.ts
@@ -36,7 +36,6 @@
         }
         case "Clear": {
           result.textContent = "0";
-          console.log("Clear");
           break;
         }
         case "=": {
@@ -65,52 +64,44 @@
         num = 1 / num;
         result.textContent = "" + num;
       }
+      if (button.classList.contains("buttons__item_plus-minus")) {
+        if (result.textContent[0] === "-") {
+          result.textContent = result.textContent.slice(1);
+        } else {
+          result.textContent = "-" + result.textContent;
+        }
+      }
     });
   });
 
   document.addEventListener('keypress', (event) => {
     switch (event.key) {
-      case "0" : {
-        console.log("Press - " + event.key);
+      case "0" :
+      case "1" :
+      case "2" :
+      case "3" :
+      case "4" :
+      case "5" :
+      case "6" :
+      case "7" :
+      case "8" :
+      case "9" :
+      case "+" :
+      case "-" :
+      case "/" :
+      case "*" :
+      case "." :
+      {
+        if (result.textContent === "0") {
+          result.textContent = "";
+        }
+        result.textContent += event.key;
         break;
       }
-      case "1" : {
-        console.log("Press - " + event.key);
+      case "Enter": {
+        result.textContent = eval(result.textContent);
         break;
       }
-      case "2" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-      case "3" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-      case "4" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-      case "5" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-      case "6" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-      case "7" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-      case "8" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-      case "9" : {
-        console.log("Press - " + event.key);
-        break;
-      }
-
     }
   });
 
